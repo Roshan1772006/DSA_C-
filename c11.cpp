a0f884ad1df71d86fbbd96a3a401add9cf553f3a
Roshan1772006 <roshankaduskar6@gmail.com>
2026-06-03 19:01:25 +0530

    Add files via upload

---
+++ c11.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+        // x++ post increment
+        // pehle use karo then increase
+        int x = 10;
+        cout<< x++<<endl;
+        cout<<x<<endl;
+
+        //++x pre increment
+        // pehle increase then use
+        cout<<++x<<endl;
+        cout<<x<<endl;
+
+        //x-- post decrement
+        // pehle use karo then decrease
+        cout<< x-- <<endl;
+        cout<<x<<endl;
+
+        //--x pre decrement
+        // pehle decrease then use
+        cout<< --x <<endl;
+        cout<<x<<endl;
+}
