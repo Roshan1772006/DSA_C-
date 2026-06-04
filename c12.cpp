1bed5a7940142531379265159b284b84494dee5f
Roshan1772006 <roshankaduskar6@gmail.com>
2026-06-04 21:50:24 +0530

    c++

---
+++ c12.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<< (6*7==42) <<endl;
+    cout<< (8+9 == 27) <<endl;
+    cout<< (85/2 == 42) <<endl;
+    bool x = true;
+    cout<<x<<endl;
+}
