ed8d6168944602d3f87f42881f43823975e6279d
Roshan1772006 <roshankaduskar6@gmail.com>
2026-06-05 19:18:01 +0530

    Add files via upload

---
+++ c13.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    if(n%2 == 0) cout<<"Even Number"<<endl;
+    else cout<<"Odd Number"<<endl;
+    if(n%2 == 0) {
+            System.out.println("Even Number");
+        }
+        else {
+            System.out.println("Odd Number");
+        }
+}
