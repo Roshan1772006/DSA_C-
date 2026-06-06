04fdf2a9c1044f145ae1db24abf01f1ea4df0c5e
Roshan1772006 <roshankaduskar6@gmail.com>
2026-06-06 18:49:58 +0530

    Add files via upload

---
+++ c14.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a positive number  : ";
+    cin>>n;
+    if(n%5 == 0) cout<<"divisible by 5"<<endl;
+    else cout<<"not divisible by 5"<<endl;
+}
