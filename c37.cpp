b43f73bb9ea703c57821e3e2e26a7afff1a64be2
Roshan1772006 <roshankaduskar6@gmail.com>
2026-07-07 10:14:15 +0530

    Add files via upload

---
+++ c37.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+        int n ;
+        cout << "Enter the number of terms : ";
+        cin >> n;
+        for(int a=99; a>103-4*n; a-=4 ){
+            System.out.println(a);
+        }
+    }
